9515383f00acda4a0d4c6d0885cf4b341d96c3d3
lobo <loboandre@hotmail.com>
2023-05-10 11:42:17 -0400

    feat: email service

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 require('dotenv').config();
+const nodemailer = require('nodemailer');
+
 
 
 exports.signup = async (req, res, next) => {
@@ -10,13 +12,43 @@ exports.signup = async (req, res, next) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ name, email, password: hashedPassword });
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    
-    res.status(201).json({ user, token });
+
+    // Send a welcome email to the user
+    await sendWelcomeEmail(email, name);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user
+      }
+    });
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ error })
   };
 };
+
+async function sendWelcomeEmail(email, name) {
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+      user: 'xonga73@gmail.com',
+      pass: 'Democracia70x07@'
+    }
+  });
+
+  let mailOptions = {
+    from: 'SysCam <xonga73@gmail.com>',
+    to: email,
+    subject: 'Bem vindo ao SysCam ',
+    text: `Obrigado por ser registrar como usuário ${name}!`
+  };
+
+  await transporter.sendMail(mailOptions);
+}
 
 
 exports.login = async (req, res, next) => {
